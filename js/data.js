96d0b6adc4d2ceebba65c87d0492602b1b0651d9
Timothé <rondeautimothe@gmail.com>
2021-03-29 09:57:12 +0200

    push_CameraMove

--- js/data.js
+++ js/data.js
@@ -1,53 +1,70 @@
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+var controls;
 
 
 
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const scene = new THREE.Scene();
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
-      
-        
-       
-       
-        function createSphere(){
-            const geometry = new THREE.SphereGeometry(2, 16, 16);
-            const material = new THREE.MeshBasicMaterial({
-                color: 0x00ff99
-            });
-            const circle = new THREE.Mesh(geometry, material);
-        
-            scene.add(circle);
-            circle.position.set(2,1,1);
-        }
-        
-        camera.position.z = 8;
-      
-      
-       /* function createCamera(){
-            controls = new THREE.OrbitControls(camera, renderer.domElement);
-            controls.enableDamping = true;
-            controls.dampingFactor = 0.05;
-            controls.rotateSpeed = 0.1;
-            invalidation.then(() => (controls.dispose(), renderer.dispose()));
-        }*/
-        
-        function createLights() {
-            // Create a directional light
-            const mainLight = new THREE.PointLight(0xffffff, 22.0);
+const sphereWidth = 2;
+const sphereHeight = 16;
+const sphereDepth = 16;
+
+const geometry = new THREE.SphereGeometry(sphereWidth, sphereHeight, sphereDepth);
+
+function createSphere(geometry, color, x) {
+  
+    const material = new THREE.MeshBasicMaterial({
+        color: color
+    });
+    const sphere = new THREE.Mesh(geometry, material);
+
+    scene.add(sphere);
+    sphere.position.set(2, 1, 1);
+    sphere.position.x = x;
+    return sphere;
+}
+
+camera.position.z = 8;
+
+const spheres = [
+    createSphere(geometry, 0x44aa88,  0),
+    createSphere(geometry, 0x8844aa, -5),
+    createSphere(geometry, 0xaa8844,  5),
+];
+
+function createCamera() {
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+    controls.rotateSpeed = 0.1;
     
+}
+
+function createLights() {
+    // Create a directional light
+    const mainLight = new THREE.PointLight(0xffffff, 22.0);
+
+
+    // add the light to the scene
+    scene.add(mainLight);
+}
+function animate(){
+
+requestAnimationFrame(animate)
+controls.update();
+renderer.render(scene, camera);
+}
+
+function init() {
+
+    // Set the background color
+    scene.background = new THREE.Color('#3c415c');
     
-            // remember to add the light to the scene
-            scene.add( mainLight);
-        }
-        function init() {
-          
-    
-            // Set the background color
-            scene.background = new THREE.Color('#00DDDC');
-            createSphere();
-          // createCamera();
-            createLights();
-            renderer.render( scene, camera );
-        }
-        init();
+    createCamera();
+    createLights();
+   
+}
+init();
+animate();
